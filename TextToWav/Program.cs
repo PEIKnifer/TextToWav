2f812f798838e2e3a2ad59532d0bdc8d9dcd982d
546964794 <546964794@qq.com>
2019-02-28 17:49:26 +0800

    首次提交

---
+++ TextToWav/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToWav
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string folderPath="";
+            try
+            {
+                folderPath = ReadData(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine("配置文件获取失败。。。");
+            }
+            try
+            {
+                if (folderPath.Length <= 0)
+                {
+                    Console.WriteLine("操作字符串空错误。。。");
+                }
+                TextToWavToolClass tool = new TextToWavToolClass(folderPath);
+            }
+            catch
+            {
+                Console.WriteLine("文件生成失败。。。");
+            }
+    Console.ReadLine();
+        }
+
+        public static string ReadData(string txtPath)
+        {
+            //C#读取TXT文件之建立  FileStream 的对象,说白了告诉程序,   
+            //文件在那里,对文件如何 处理,对文件内容采取的处理方式   
+            StreamReader sr=null;
+            FileStream fs=null;
+            try
+            {
+                fs = new FileStream(txtPath, FileMode.Open, FileAccess.Read);
+                //仅 对文本 执行  读写操作   
+                sr = new StreamReader(fs);
+            }
+            catch
+            {
+
+            }
+            //定位操作点,begin 是一个参考点   
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            //读一下，看看文件内有没有内容，为下一步循环 提供判断依据   
+            //sr.ReadLine() 这里是 StreamReader的要领  可不是 console 中的~    
+            string str = sr.ReadToEnd();//假如  文件有内容    
+            //while (str != null)
+            //{
+            //    //输出字符串，str 在上面已经定义了 读入一行字符    
+            //    Console.WriteLine("{0}", str);
+            //    //这里我的理会是 当输出一行后，指针移动到下一行~   
+            //    //下面这句话就是 判断 指针所指这行能无法 有内容~   
+            //    str = sr.ReadLine();
+            //}
+            ////C#读取TXT文件之关上文件，留心顺序，先对文件内部执行 关上，然后才是文件~   
+            sr.Close();
+            fs.Close();
+            return str;
+        }   
+        
+        
+    }
+}
---
+++ TextToWav/TextToWavToolClass.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToWav
+{
+    public class TextToWavToolClass
+    {
+
+        /// <summary>
+        /// 音量
+        /// </summary>
+        private int value = 100;
+        /// <summary>
+        /// 语速
+        /// </summary>
+        private int rate;
+
+        FileStream file;
+
+        string filePath;
+
+        List<string> operationText;
+
+        SpeechSynthesizer synth;
+
+        string folderName="OutPut";
+
+        string baseFolderName;
+
+        public TextToWavToolClass(string toolText)
+        {
+            baseFolderName = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            if (!Directory.Exists(baseFolderName + folderName))
+            {
+                Directory.CreateDirectory(baseFolderName + folderName);
+            }
+            operationText = new List<string>();
+            string toolString = "";
+            try
+            {
+                synth = new SpeechSynthesizer();   //创建语音实例
+                synth.Rate = -1; //设置语速,[-10,10]
+                synth.Volume = 100; //设置音量,[0,100]
+                while (operationText.Count > 0)
+                {
+                    operationText.RemoveAt(0);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("语音处理类初始化失败!!");
+            }
+            for (int i = 0; i < toolText.Length; i++)
+            {
+                if (toolText[i] == '\n' || i == toolText.Length - 1)
+                {
+                    if (i == toolText.Length - 1)
+                    {
+                        toolString += toolText[toolText.Length - 1];
+                    }
+                    if (toolString.Trim().Length == 0)
+                    {
+                        Console.WriteLine("空内容无法生成!", "错误提示");
+                        return;
+                    }
+                    operationText.Add(toolString);
+                    toolString = "";
+                }
+                else
+                {
+                    //if (textBox1.Text[i] == 'n')
+                    //{
+
+                    //}
+                    //else
+                    toolString += toolText[i];
+                }
+            }
+
+            SaveFile(operationText);
+            Console.WriteLine("工具处理结束");
+        }
+
+        /// <summary>
+        /// 生成语音文件的方法
+        /// </summary>
+        /// <param name="text"></param>
+        private void SaveFile(List<string> text)
+        {
+            synth = new SpeechSynthesizer();
+
+            for (int i = 0; i < text.Count; i++)
+            {
+                StringBuilder titleBuilder = new StringBuilder(text[i]);
+                foreach (char rInvalidChar in Path.GetInvalidFileNameChars())
+                    titleBuilder = titleBuilder.Replace(rInvalidChar.ToString(), string.Empty);
+
+                string fileNameStr = "";
+                if (titleBuilder.ToString().Length > 4)
+                {
+                    for (int t = 0; t < 4; t++)
+                    {
+                        fileNameStr += titleBuilder.ToString()[t];
+                    }
+                }
+                else
+                {
+                    for (int t = 0; t < titleBuilder.ToString().Length; t++)
+                    {
+                        fileNameStr += titleBuilder.ToString()[t];
+                    }
+                }
+
+                filePath = baseFolderName + folderName+@"\" +"ID"+i+"_"+ fileNameStr + ".wav";
+                //filePath=Application.LocalUserAppDataPath + text[i] + ".wav";
+
+                Console.WriteLine("在以下路径准备创建  " + filePath);
+                file = File.Create(filePath);
+                file.Close();
+
+                synth.SetOutputToWaveFile(filePath);
+                synth.Volume = value;
+                synth.Rate = rate;
+
+                synth.Speak(operationText[i]);
+                synth.SetOutputToNull();
+
+
+                Console.WriteLine("生成成功!在" + filePath + "路径中！", "提示");
+                Console.WriteLine("File Build Complete %"+(int)((100/(float)text.Count)* (i+1) )+ "...");
+            }
+        }
+    }
+}
